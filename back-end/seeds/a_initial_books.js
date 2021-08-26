7f6e8a8eb44b9caa02e05da7dc68b311972139ef
Ty Jereza <nexusspark.jerezt@gmail.com>
2021-08-26 06:59:58 -0700

    Edit seed file for books

--- back-end/seeds/a_initial_books.js
+++ back-end/seeds/a_initial_books.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('books').insert([
         {book_title: 'The Great Gatsby', author:'Scooby', isbn_number:'01', is_checked_out:true},
-        {book_title: 'The Art of War', author:'Ty', isbn_number:'02', is_checked_out:true},
+        {book_title: 'The Art of War', author:'Ty', isbn_number:'02', is_checked_out:false},
         {book_title: 'The War of Art', author:'Lincoln', isbn_number:'03', is_checked_out:false},
         {book_title: 'Frendle', author:'Putin', isbn_number:'04', is_checked_out:false},
         {book_title: 'The Cat in the Hat', author:'Earnest Hemmingway', isbn_number:'015', is_checked_out:false},
